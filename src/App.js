5f483d1fdfeb0e187f245c5b443dddbe42ff8f3c
Rungrawee <rmusignawabut@gmail.com>
2021-08-21 03:50:00 +0700

    version1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react'
+import Dropdown from './components/Dropdown';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import { SliderData } from './data/SliderData';
+import GlobalStyle from './globalstyles';
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle/>
+       <Navbar toggle={toggle}/>
+       <Dropdown isOpen={isOpen} toggle={toggle}/>
+       <Hero slides={SliderData}/>
+    </>
   );
 }
 
---
+++ src/data/SliderData.js
@@ -0,0 +1,39 @@
+import ImageOne from '../images/james-bold-loGgEYdYMLk-unsplash.jpg'
+import ImageTwo from '../images/jan-pictures-2TCIJsN9iXM-unsplash.jpg'
+import ImageThree from '../images/stefan-maass-m0S7wjnvKRM-unsplash.jpg'
+import ImageFour from '../images/wedding-photography-CgR5Ey7LNAE-unsplash.jpg'
+
+export const SliderData = [
+    {
+        title: 'Wedding with Garden', 
+        prices: '$42,790',
+        path: '/homes',
+        label: 'See More',
+        images: ImageOne,
+        alt: 'House'
+    },
+    {
+        title: 'Wedding Room Luxurios', 
+        prices: '$68,430',
+        path: '/homes',
+        label: 'See More',
+        images: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'Gadget with Impressive', 
+        prices: '$6,825',
+        path: '/homes',
+        label: 'See More',
+        images: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'Love In buttom Flowers', 
+        prices: '$26,830',
+        path: '/homes',
+        label: 'See More',
+        images: ImageFour,
+        alt: 'House'
+    }
+]
